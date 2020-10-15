c83d019ae284a9eeff642ffe8517f31e68ac7989
ddomi415 <danijel.4155@gmail.com>
2020-10-15 08:52:38 +0200

    ss

---
+++ Läxa1/laxa1.js
@@ -0,0 +1,14 @@
+console.log ("hej")
+
+let namn = "Danijel"
+
+console.log (`hej ${namn}`)
+
+let ålder = prompt("Ange din ålder: ")
+
+console.log (`jag är  ${ålder} år gammal`)
+
+let adress = prompt("ang din adress: ")
+
+console.log (` min ${adress} `)
+
---
+++ funktion.html/funktion1.js
@@ -0,0 +1,21 @@
+let name = " Domi"
+
+
+funktion sayhello(){
+    console.log( hej $(namn)`)
+
+}
+// anropar funktion sayhello
+sayhello()
+sayhello()
+console.log(` min ålder är $(alder)´) - alder är inte synlig här
+
+funktion addera(tal1,tal2){
+    let summa = tal1+tal2
+    return summa 
+
+}
+
+console.log(`2+5=$(addera(2,5)`)
+
+// skapa en subtrationfunktion
